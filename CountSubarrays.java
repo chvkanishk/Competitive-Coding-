3a695b68d98e103eb54195d046bc01237a3cb25a
Kanishk <vchaganti2@horizon.csueastbay.edu>
2025-05-09 15:31:31 -0700

    subarray

---
+++ CountSubarrays.java
@@ -0,0 +1,22 @@
+class Solution{
+    public int count(int[] nums){
+        int n= 0;
+        for(int i = 0; i < nums.length-2; i++) {
+            if (2 * (nums[i] + nums[i + 2]) == nums[i + 1]) {
+                n++;
+            }
+        }
+        return n;
+    }
+}
+
+
+public class CountSubarrays {
+    public static void main(String[] args) {
+        int[] nums= {1,2,1,4,1}; 
+
+        Solution s = new Solution();
+        System.err.println(s.count(nums));
+    }
+    
+}
